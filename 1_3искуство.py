ef9f4f58367f1a94ba6490083492ac089153fe1b
kirill35476 <tr.kirilllll@gmail.com>
2025-02-09 13:47:31 +0300

    add

--- 1_3искуство.py
+++ 1_3искуство.py
@@ -1,21 +1,41 @@
 import pygame
+from all_colors import *
+import random
 pygame.init()
 
-size = (1200, 720)
-screen = pygame.display.set_mode(size)
+pygame.mixer.init()
+pygame.mixer.music.load('resources/Hydrogen.mp3')
+pygame.mixer.music.play(-1)
+
+size = (0, 0)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 pygame.display.set_caption("новая игра")
-BACKGROUND = (255,255,255)
+BACKGROUND = (255, 255, 255)
 screen.fill(BACKGROUND)
-FPS = 60
+# FPS = 1
 clock = pygame.time.Clock()
+
+COLORS = [BLACK, WHITE, RED, GREEN, YELLOW, CYAN, MAGENTA, GRAY,
+          ORANGE, PINK, BROWN, PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL, COLD]
+
 running = True
+timer = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(BACKGROUND)
-    pygame.display.flip()
-    clock.tick(FPS)
+    screen.fill(random.choice(COLORS))
+    pygame.time.delay(random.randint(200, 200))
+
+    for _ in range(10):
+        x = random.randint(0, 1000)
+        y = random.randint(0, 1000)
+        radius = random.randint(10, 100)
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        pygame.draw.circle(screen, color, (x, y), radius)
+
+        pygame.display.flip()
 
 pygame.quit()
