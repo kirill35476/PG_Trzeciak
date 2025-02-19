04634399e75b8746599fc3f617817e08bd520cf5
kirill35476 <tr.kirilllll@gmail.com>
2025-02-19 19:40:39 +0300

    add

--- 2_4.py
+++ 2_4.py
@@ -1,56 +1,42 @@
-from all_colors import *
-from pygame.constants import *
-from random import *
 import pygame
+from random import choice
+from pygame.time import Clock
+from all_colors import *
+
 pygame.init()
 
-size = (1200, 720)
-screen = pygame.display.set_mode(size)
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("новая игра")
-BACKGROUND = (255,255,255)
+BACKGROUND = (255, 255, 255)
 screen.fill(BACKGROUND)
 
-x =0
-y =0
-rect_size =  200
+
 COLORS = [BLACK, WHITE, RED, GREEN, YELLOW, CYAN, MAGENTA, GRAY,
           ORANGE, PINK, BROWN, PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL, COLD]
 
-# rect1 = pygame.Rect(x,y,rect_size,rect_size)
-# rect1.center = (screen.get_width()//2, screen.get_height()//2)
-# pygame.draw.rect(screen,BLACK,rect1)
-# rect2 = pygame.Rect(x,y,rect_size//2,rect_size//2)
-# rect2.center = (screen.get_width()//2, screen.get_height()//2)
-# pygame.draw.rect(screen,RED,rect2)
 
-for i in range(3):
-    rect = pygame.Rect(x,y,rect_size,rect_size)
-    rect.center = (screen.get_width()//2, screen.get_height()//2)
-    pygame.draw.rect(screen, choice(colors),rect)
+initial_size = 200
 
-    x += rect_size
-    if x > screen.get_width():
-        x = 0
-        y += rect_size
-        if y > screen.get_height():
-            y = 0
-
-FPS = 60
-clock = pygame.time.Clock()
 running = True
+clock = Clock()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
-
-
-
-
-
+    size = initial_size
+    for i in range(18):
+        rect = pygame.Rect(0, 0, size, size)
+        rect.center = (screen_width // 2, screen_height // 2)
+        pygame.draw.rect(screen, choice(COLORS), rect)
+        size -= 15
 
     pygame.display.flip()
-    clock.tick(FPS)
+
+    clock.tick(5)
 
 pygame.quit()
---
+++ 2_5.py
@@ -0,0 +1,52 @@
+import pygame
+from all_colors import *
+from random import *
+pygame.init()
+
+width_screen = 640
+height_screen = 480
+screen = pygame.display.set_mode((width_screen, height_screen))
+pygame.display.set_caption("новая игра")
+BACKGROUND = (255, 255, 255)
+screen.fill(BACKGROUND)
+
+width = 100
+height = 75
+
+COLORS = [BLACK, WHITE, RED, GREEN, YELLOW, CYAN, MAGENTA, GRAY,
+          ORANGE, PINK, BROWN, PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL, COLD]
+
+rects = []
+
+rects.append(pygame.Rect(0, 0, width, height))
+
+rects.append(pygame.Rect(0, 0, width, height))
+rects[-1].topright = (width_screen, 0)
+
+rects.append(pygame.Rect(0, 0, width, height))
+rects[-1].bottomleft = (0, height_screen)
+
+rects.append(pygame.Rect(0, 0, width, height))
+rects[-1].bottomright = (width_screen, height_screen)
+
+rects.append(pygame.Rect(0, 0, width, height))
+rects[-1].center = (width_screen//2, height_screen//2)
+
+for rect in rects:
+    pygame.draw.rect(screen, choice(COLORS), rect)
+    pygame.time.delay(randint(200, 200))
+
+
+pygame.display.flip()
+clock = pygame.time.Clock()
+timer = 0
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BACKGROUND)
+    clock.tick()
+
+pygame.quit()
